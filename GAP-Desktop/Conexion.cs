07253144e1b274ee4528c5c367c8b08a036580e2
ManuelCG23 <jjrcamachogomez@gmail.com>
2021-06-10 20:27:50 +0200

    Versión 1.5

--- GAP-Desktop/Conexion.cs
+++ GAP-Desktop/Conexion.cs
@@ -10,6 +10,6 @@ namespace PruebaC
     class Conexion
     {
         //public static MySqlConnection conexion = new MySqlConnection("datasource= 2905900; Database=manuelcamacho; Uid=manuelcamacho; Pwd=8pUl7i2$;");
-        public static MySqlConnection conexion = new MySqlConnection("datasource=127.0.0.1;port=3307;Database=proyecto_gap; Uid=root;");
+        public static MySqlConnection conexion = new MySqlConnection("datasource=127.0.0.1;port=3306;Database=proyecto_gap; Uid=root;");
     }
 }
